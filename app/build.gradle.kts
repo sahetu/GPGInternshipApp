a8ebe2fc86ddf8454ef9e0a1515be9f1b0f904d6
Sagar Khatri <khatrisagar2@gmail.com>
2025-01-23 11:16:05 +0530

    Responsive UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    //Responsive UI Library
+    //implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation(libs.sdp)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
