17874a2ad48e638e2bd392bcd323c7a20cffb5de
Sagar Khatri <khatrisagar2@gmail.com>
2025-01-24 12:53:47 +0530

    Login Java Changes

--- app/src/main/java/gpg/internship/MainActivity.java
+++ app/src/main/java/gpg/internship/MainActivity.java
@@ -1,6 +1,12 @@
 package gpg.internship;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,54 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    public static EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        username = findViewById(R.id.main_username);
+        password = findViewById(R.id.main_password);
+
+        login = findViewById(R.id.main_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().trim().equals("")){
+                    //username.setError(getResources().getString(R.string.login));
+                    username.setError("Username Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Char Password Required");
+                }
+                else {
+                    System.out.println("Login Successfully");
+                    Log.d("RESPONSE", "Login Successfully");
+                    Log.e("RESPONSE", "Login Successfully");
+                    Log.w("RESPONSE", "Login Successfully");
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
+                    Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("USERNAME",username.getText().toString());
+                    bundle.putString("PASSWORD",password.getText().toString());
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+
+                }
+            }
         });
+
     }
 }
